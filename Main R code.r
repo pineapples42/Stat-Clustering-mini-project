5bae83c3e2668f645268f61a73ecfc302017d79e
pineapples42 <32442856+pineapples42@users.noreply.github.com>
2017-10-03 18:47:17 -0400

    Update and rename Main R code to Main R code.r

---
+++ Main R code.r
@@ -0,0 +1,18 @@
+library(flowCore)
+# Info for all 12 people will be stored in the 'people' variable in dataframe format
+# EXAMPLES: 
+#     instead of summary(person5), use summary(people[[5]]) (double square brackets are neccesary)
+#     people[[5]]$FSC.H
+#     kmeans(people[[5]], 3)
+# Add your own folder path in folder 
+folder = '/home/persimmon/Documents/project/'
+people = list()
+file = '1'
+ext = '.fcs'
+for(i in 1:9){
+  people = append(people, list(as.data.frame(read.FCS(as.character(paste(c(folder,'00',file,ext), collapse = '')))@exprs)))
+  file =gsub(as.character(i), as.character(i+1), file)
+             }
+people = append(people, list(as.data.frame(read.FCS(as.character(paste(c(folder,'010.fcs'), collapse = '')))@exprs)))
+people = append(people, list(as.data.frame(read.FCS(as.character(paste(c(folder,'011.fcs'), collapse = '')))@exprs)))
+people = append(people, list(as.data.frame(read.FCS(as.character(paste(c(folder,'012.fcs'), collapse = '')))@exprs)))
