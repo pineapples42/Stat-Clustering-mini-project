84c92050934e45453414f6cb68629dbaf36a327b
davidazizi <32463397+davidazizi@users.noreply.github.com>
2017-10-07 20:45:39 -0400

    Lots of fun stuff

--- Clusters.R
+++ Clusters.R
@@ -8,6 +8,27 @@ for (i in 1:12) {
 	nlevels <- c(nlevels, nlevels(obj))
 }
 
+# SCALING DOESN'T WORK YET OOPS USE THE BLOCK BELOW THIS
+ # for (i in 1:12) {
+	patient <- people[[i]]
+	patient.clean <- subset(patient, Targets > 0)
+	patient.raw <- patient.clean[c("FSC.H", "SSC.H", "FL1.H", "FL2.H", "FL3.H", "FL4.H")]
+	patient.scaled <- scale(patient.raw)
+	km <- kmeans(patient.scaled, nlevels[i] - 1)
+	patient.scaled <- cbind(patient.scaled, "Predict" = km$cluster)
+	patient.scaled <- cbind(patient.scaled, patient.clean$Targets))
+	conMatrix <- table(patient.scaled["Predict"], patient.scaled["V1"])
+	retrieved <- sum(patient.scaled["Predict"])
+	precision <- sum(patient.scaled["Predict"] & patient.scaled["V1"])/retrieved
+	recall <- sum(patient.scaled["Predict"] & patient.scaled["V1"])/sum(patient.scaled["V1"])
+	Fmeasure <- 2 * precision * recall/(precision + recall)
+	print(conMatrix)
+	print(Fmeasure)
+	nmi <- external_validation(patient.scaled["V1"], patient.scaled["Predict"], method = "nmi")
+	print(nmi)
+}
+
+par(mfrow = c(3,4))
 # For each patient:
 for (i in 1:12) {
 	# Create a subset for the patient
@@ -16,22 +37,22 @@ for (i in 1:12) {
 	patient.clean <- subset(patient, Targets > 0)
 	# Subset for use in kmeans
 	patient.raw <- patient.clean[c("FSC.H", "SSC.H", "FL1.H", "FL2.H", "FL3.H", "FL4.H")]
-	# Scale the data for cleaner results
-	patient.scaled <- scale(patient.raw)
 	# Run kmeans using nlevels vector from earlier
-	km <- kmeans(patient.scaled, nlevels[i] - 1)
+	km <- kmeans(patient.raw, nlevels[i] - 1)
 	# Add kmeans predictions to scaled dataframe
-	patient.scaled$Predicted <- km$cluster
+	patient.raw$Predict <- km$cluster
 	# Return actual values to scaled dataframe
-	patient.scaled$Targets <- patient.clean$Targets
+	patient.raw$Actual <- patient.clean$Targets
 	# Create a confusion matrix for the dataframe & print it
-	conMatrix <- table(patient.scaled$Predicted, patient.scaled$Targets)
-	retrieved <- sum(patient.scaled$Predicted)
-	precision <- sum(patient.scaled$Predicted & patient.scaled$Targets)/retrieved
-	recall <- sum(patient.scaled$Predicted & patient.scaled$Targets)/sum(patient.scaled$Targets)
+	conMatrix <- table(patient.raw$Predict, patient.raw$Actual)
+	retrieved <- sum(patient.raw$Predict)
+	precision <- sum(patient.raw$Predict & patient.raw$Actual)/retrieved
+	recall <- sum(patient.raw$Predict & patient.raw$Actual)/sum(patient.raw$Actual)
 	Fmeasure <- 2 * precision * recall/(precision + recall)
 	print(conMatrix)
 	print(Fmeasure)
-	nmi <- external_validation(patient.scaled$Targets, patient.scaled$Predicted, method = "nmi")
+	nmi <- external_validation(patient.raw$Actual, patient.raw$Predict, method = "nmi")
 	print(nmi)
+	scatterplot3d(patient.raw$FSC.H, patient.raw$SSC.H, patient.raw$FL1.H, color=patient.raw$Predict, pch=patient.raw$Actual)
+	# plot(patient.raw$FSC.H, patient.raw$SSC.H, col=patient.raw$Predict, pch=patient.raw$Actual)
 }
