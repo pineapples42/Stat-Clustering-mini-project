045417e3cdc8c9ec81ac6a7d939a8ceb6887c326
davidazizi <32463397+davidazizi@users.noreply.github.com>
2017-10-11 18:17:36 -0400

    Added new NMI evaluation of everyone cluster

--- David.r
+++ David.r
@@ -1,4 +1,87 @@
-# load the datframe and packages first
+####
+# NMI for patients when clustered individually
+global_nmi <- c()
+for (index in 1:12) {
+  local_nmi <- c()
+  for (count in 1:20) {
+    person.raw <- people[[index]][, 1:6]
+    k <- nlevels(factor(people[[index]]$Targets))
+    km <- kmeans(person.raw, k)
+    nmi <- external_validation(people[[index]]$Targets, km$cluster, method = "nmi")
+    local_nmi <- c(local_nmi, nmi)
+  }
+  global_nmi <- c(global_nmi, mean(local_nmi))
+}
+
+# Binding everyone and then dividing
+everyone = people[[1]]
+for(i in 2:12){
+  everyone = rbind(everyone, people[[i]])
+}
+
+ranges <- list(1:13831, 13832:28767, 28768:43994, 43995:56745, 56746:73753, 73754:88559, 88560:102332, 102333:117119, 117120:134759, 134760:158136, 158137:190836, 190837:207171)
+everyone_nmi <- c()
+
+km.all <- kmeans(everyone[ , 1:6], 5)
+for(range in ranges){
+  nmi <- external_validation(km.all$cluster[range], everyone$Targets[range], method = "nmi")
+  everyone_nmi <- c(everyone_nmi, nmi)
+}
+
+
+height <- rbind(global_nmi, everyone_nmi)
+
+colours <- c("dodgerblue1", "darkorange")
+
+
+mp <-
+  barplot(
+    height,
+    beside = TRUE,
+    col = colours,
+    names.arg = 1:12,
+    main = "NMI, Grouped v. Individual",
+    ylab = "NMI",
+    ylim = c(0, 1)
+  )
+
+text(mp, height, labels = format(height, 4),
+     pos = 3, cex = .75)
+
+####
+
+# load flowset and dataframe
+SS <- SamSPECTRAL(flowset$`001.fcs`@exprs, normal.sigma = 200, separation.factor = 0.5)
+nmi <- external_validation(SS, people[[1]]$Targets, method = "nmi")
+
+
+####
+poly = function(a){
+  c = 7
+  for(i in 1:6){
+    for(j in 1:6){
+      a[,c] = a[,j] * a[,i]
+      c = c+1
+    }
+  }
+  return(a)
+}
+
+global_nmi <- c()
+for (index in 1:12) {
+  local_nmi <- c()
+  for (count in 1:20) {
+    person.raw <- people[[index]][, 1:6]
+    person.pf <- poly(person.raw)
+    k <- nlevels(factor(people[[index]]$Targets))
+    km <- kmeans(person.pf, k)
+    nmi <- external_validation(people[[index]]$Targets, km$cluster, method = "nmi")
+    local_nmi <- c(local_nmi, nmi)
+  }
+  global_nmi <- c(global_nmi, mean(local_nmi))
+}
+
+
 
 # For one patient:
 patient <- people[[10]]
@@ -16,12 +99,12 @@ patient.raw <-
 patient.raw <- as.data.frame(scale(patient.raw))
 	
 	
-k <- nlevels(factor(patient.clean$Targets))
+k <- nlevels(factor(patient$Targets))
 km <- kmeans(patient.raw, k)
 	
 patient.new <- as.data.frame(patient.raw[,1:6])
 patient.new$Predict <- km$cluster
-patient.new$Actual <- as.integer(patient.clean$Targets)
+patient.new$Actual <- as.integer(patient$Targets)
 	
 print(paste("Patient 10"))
 
@@ -29,7 +112,6 @@ conMatrix <- table(patient.new$Predict, patient.new$Actual)
 print(conMatrix)
 
 nmi <- external_validation(patient.new$Actual, patient.new$Predict, method = "nmi")
-nmilist <- c(nmilist, nmi)
 print(nmi)
 	
 patient.sample <- patient.new[sample(nrow(patient.new), 300),]
@@ -45,7 +127,6 @@ scatterplot3d(
     ylab = "SSC.H",
     zlab = "FL1.H"
 )
-
 
 
 # This takes Kmeans of the polynomial feature augmented matrix
@@ -227,7 +308,6 @@ print(Fmeasure)
 nmi <-
   external_validation(patient.scaled["V1"], patient.scaled["Predict"], method = "nmi")
 print(nmi)
-}
 
 
 # THIS IS THE BLOCK THAT WORKS
