ada26ae70d46bf2e3b892ae8d775132ab22c111e
davidazizi <32463397+davidazizi@users.noreply.github.com>
2017-10-12 12:50:35 -0400

    last minute stuff

--- David.r
+++ David.r
@@ -573,3 +573,68 @@ for (i in 1:12) {
   # plot(patient.raw$FSC.H, patient.raw$SSC.H, col=patient.raw$Predict, pch=patient.raw$Actual)
 }
 mean(preprocess)
+
+
+## FROM MAIN
+# Updates people dataframe with new predicted and actual scores.
+f.measure <- function(predicted, true) {
+  retrieved <- sum(predicted)
+  precision <- sum(predicted & true)/retrieved
+  recall <- sum(predicted & true)/sum(true)
+  fmeasure <- 2 * precision * recall/(precision + recall)
+  return(fmeasure)
+}
+
+library(ClusterR)
+library(scatterplot3d)
+par(mfrow = c(3,4))
+for (i in 1:12){
+  patient <- people[[i]]
+  patient.clean <- subset(patient, Targets > 0)
+  patient.raw <-
+    patient.clean[c("FSC.H", "SSC.H", "FL1.H", "FL2.H", "FL3.H", "FL4.H")]
+  
+  k <- nlevels(factor(patient.clean$Targets))
+  km <- kmeans(patient.raw, k)
+  
+  patient.new <- patient.raw
+  patient.new$Predict <- km$cluster
+  patient.new$Actual <- as.integer(patient.clean$Targets)
+  
+  patient.ztemp <- subset(patient, Targets == 0)
+  patient.z <-
+    patient.ztemp[c("FSC.H", "SSC.H", "FL1.H", "FL2.H", "FL3.H", "FL4.H")]
+  patient.z$Actual <- as.integer(patient.ztemp$Targets)
+  patient.z$Predict <- integer(length(patient.z$Actual))
+  
+  patient.new <- rbind(patient.new, patient.z)
+  
+  people[[i]] <- patient.new
+  
+  conMatrix <- table(patient.new$Predict, patient.new$Actual)
+  print(conMatrix)
+  
+  fmeasure <- f.measure(patient.new$Predict, patient.new$Actual)
+  print(fmeasure)
+  
+  nmi <- external_validation(patient.new$Actual, patient.new$Predict, method = "nmi")
+  print(nmi)
+  
+  scatterplot3d(
+    patient.new$FSC.H,
+    patient.new$SSC.H,
+    patient.new$FL1.H,
+    color = patient.new$Predict,
+    pch = patient.new$Actual,
+    main = paste("Patient", i, sep = " "),
+    xlab = "FSC.H",
+    ylab = "SSC.H",
+    zlab = "FL1.H"
+  )
+}
+
+# Save patient data in separate files
+folder = '/Users/david/Documents/GitHub/Stat-Clustering-mini-project/Patient Data/'
+for(i in 1:12){
+  save(people[[i]], file = paste(folder, "Patient", i, ".Rdata", sep=""))
+}
--- Main R code.r
+++ Main R code.r
@@ -10,20 +10,26 @@ folder = '/Users/david/Documents/GitHub/Stat-Clustering-mini-project/FlowCore Da
 people = list()
 file = '1'
 ext = '.fcs'
-for(i in 1:9){
+for (i in 1:9) {
   people = append(people, list(as.data.frame(read.FCS(as.character(paste(c(folder,'00',file,ext), collapse = '')))@exprs)))
-  file =gsub(as.character(i), as.character(i+1), file)
+  file = gsub(as.character(i), as.character(i+1), file)
              }
 people = append(people, list(as.data.frame(read.FCS(as.character(paste(c(folder,'010.fcs'), collapse = '')))@exprs)))
 people = append(people, list(as.data.frame(read.FCS(as.character(paste(c(folder,'011.fcs'), collapse = '')))@exprs)))
 people = append(people, list(as.data.frame(read.FCS(as.character(paste(c(folder,'012.fcs'), collapse = '')))@exprs)))
 
 # Elbow Plots (Determining K if we didn't have the manual gates)
+nlevels <- c()
+for (i in 1:12) {
+  obj <- factor(people[[i]]$Targets)
+  nlevels <- c(nlevels, nlevels(obj))
+}
+
 scores = list()
 clusters = list()
 for(person in people){
     kmscore <- c()
-    for (k in 1:30) {
+    for (k in 1:20) {
         km <- kmeans(person, k, iter.max = 20)
         kmscore <- c(kmscore, km$betweenss / km$totss)
 	}
@@ -31,7 +37,12 @@ for(person in people){
 }
 par(mfrow = c(3,4))
 for(i in 1:12){
-	plot(scores[[i]])
+  plot <- plot(scores[[i]],
+       type = "p",
+       ylab = "Between SS / Total SS",
+       xlab = "Number of Clusters",
+       main = paste("Patient", i, sep = ' '))
+  plot <- points(nlevels[i], scores[[i]][nlevels[i]], col = "red", pch = 3)
 }
 
 #Adding manual gated results to dataframe
@@ -47,65 +58,197 @@ targets = as.matrix(read.csv(paste(c(folder,'012.csv'), collapse = '')))
 people[[12]]$Targets = targets
 
 # Running Kmeans with the factor levels given by the manual gates.
-# Updates people dataframe with new predicted and actual scores.
-f.measure <- function(predicted, true) {
-	retrieved <- sum(predicted)
-	precision <- sum(predicted & true)/retrieved
-	recall <- sum(predicted & true)/sum(true)
-	fmeasure <- 2 * precision * recall/(precision + recall)
-	return(fmeasure)
+poly = function(a) {
+  c = 7
+  for (i in 1:6) {
+    for (j in 1:6) {
+      a[, c] = a[, j] * a[, i]
+      c = c + 1
+    }
+  }
+  return(a)
 }
 
-library(ClusterR)
-library(scatterplot3d)
-par(mfrow = c(3,4))
-for (i in 1:12){
-	patient <- people[[i]]
-	patient.clean <- subset(patient, Targets > 0)
-	patient.raw <-
-	  patient.clean[c("FSC.H", "SSC.H", "FL1.H", "FL2.H", "FL3.H", "FL4.H")]
-	
-	k <- nlevels(factor(patient.clean$Targets))
-	km <- kmeans(patient.raw, k)
-	
-	patient.new <- patient.raw
-	patient.new$Predict <- km$cluster
-	patient.new$Actual <- as.integer(patient.clean$Targets)
-	
-	patient.ztemp <- subset(patient, Targets == 0)
-	patient.z <-
-	  patient.ztemp[c("FSC.H", "SSC.H", "FL1.H", "FL2.H", "FL3.H", "FL4.H")]
-	patient.z$Actual <- as.integer(patient.ztemp$Targets)
-	patient.z$Predict <- integer(length(patient.z$Actual))
-	
-	patient.new <- rbind(patient.new, patient.z)
-	
-	people[[i]] <- patient.new
-	
-	conMatrix <- table(patient.new$Predict, patient.new$Actual)
-	print(conMatrix)
-	
-	fmeasure <- f.measure(patient.new$Predict, patient.new$Actual)
-	print(fmeasure)
-	
-	nmi <- external_validation(patient.new$Actual, patient.new$Predict, method = "nmi")
-	print(nmi)
-	
-	scatterplot3d(
-	  patient.new$FSC.H,
-	  patient.new$SSC.H,
-	  patient.new$FL1.H,
-	  color = patient.new$Predict,
-	  pch = patient.new$Actual,
-	  main = paste("Patient", i, sep = " "),
-	  xlab = "FSC.H",
-	  ylab = "SSC.H",
-	  zlab = "FL1.H"
-	)
-}
-
-# Save patient data in separate files
-folder = '/Users/david/Documents/GitHub/Stat-Clustering-mini-project/Patient Data/'
+global.pfeatures <- c()
 for(i in 1:12){
-	save(people[[i]], file = paste(folder, "Patient", i, ".Rdata", sep=""))
+  pfeatures <- c()
+  for (variable in 1:20){
+    k <- nlevels(factor(people[[i]]$Targets))
+    people.poly <- poly(people[[i]])
+    km <- kmeans(people.poly, k)
+    nmi <- external_validation(people[[i]]$Targets, km$cluster, method = 'nmi')
+    pfeatures <- c(pfeatures, nmi)
+  }
+  global.pfeatures <- c(global.pfeatures, mean(pfeatures))
 }
+
+global.nopfeatures <- c()
+for(i in 1:12){
+  nopfeatures <- c()
+  for (variable in 1:20){
+    k <- nlevels(factor(people[[i]]$Targets))
+    people.poly <- people[[i]][,1:6]
+    km <- kmeans(people.poly, k)
+    nmi <- external_validation(people[[i]]$Targets, km$cluster, method = 'nmi')
+    nopfeatures <- c(nopfeatures, nmi)
+  }
+  global.nopfeatures <- c(global.nopfeatures, mean(nopfeatures))
+}
+
+height.pf <- rbind(global.nopfeatures, global.pfeatures)
+
+pf <-
+  barplot(
+    height.pf,
+    beside = TRUE,
+    col = c(4,2),
+    names.arg = 1:12,
+    main = "Polynomial Features Comparison",
+    ylab = "NMI",
+    xlab = "Patient",
+    ylim = c(0, 1),
+    legend.text = c(
+      paste("No Polynomial Features, mean: ", round(mean(global.nopfeatures), 2), sep = ""),
+      paste("With Polynomial Features, mean: ", round(mean(global.pfeatures), 2), sep = "")
+    ),
+    args.legend = list(x = "top", cex = 0.75)
+  )
+
+text(
+  pf,
+  height.pf,
+  labels = format(100* round(height.pf, 2)),
+  pos = 3,
+  cex = 0.6
+)
+
+######
+global.not.scaled.nmi <- c()
+for (i in 1:12) {
+  not_scaled_nmi = c()
+  for (variable in 1:20) {
+    k <- nlevels(factor(people[[i]]$Targets))
+    km <- kmeans((people[[i]][, 1:6]), k)
+    nmi <-
+      external_validation(people[[i]]$Targets, km$cluster, method = 'nmi')
+    not_scaled_nmi = c(not_scaled_nmi, nmi)
+  }
+  global.not.scaled.nmi <- c(global.not.scaled.nmi, mean(not_scaled_nmi))
+}
+
+global.scaled.nmi <- c()
+for (i in 1:12) {
+  scaled_nmi = c()
+  for (variable in 1:20) {
+    k <- nlevels(factor(people[[i]]$Targets))
+    km <- kmeans(scale(people[[i]][, 1:6]), k)
+    nmi <-
+      external_validation(people[[i]]$Targets, km$cluster, method = 'nmi')
+    scaled_nmi = c(scaled_nmi, nmi)
+  }
+  global.scaled.nmi <- c(global.scaled.nmi, mean(scaled_nmi))
+}
+
+height.s <- rbind(global.not.scaled.nmi, global.scaled.nmi)
+
+colours <- c(4, 2)
+mp <-
+  barplot(
+    height.s,
+    beside = TRUE,
+    col = colours,
+    names.arg = 1:12,
+    main = "Scaled vs. Unscaled Comparison",
+    ylab = "NMI",
+    xlab = "Patient",
+    ylim = c(0, 1),
+    legend.text = c(
+      paste("Unscaled, mean: ", round(mean(global.not.scaled.nmi), 2), sep = ""),
+      paste("Scaled, mean: ", round(mean(global.scaled.nmi), 2), sep = "")
+    ),
+    args.legend = list(x = "top", cex = 0.75)
+  )
+
+text(
+  mp,
+  height.s,
+  labels = format(100 * round(height.s, 2)),
+  pos = 3,
+  cex = 0.6
+  
+)
+
+####
+# NMI for patients when clustered individually
+global_nmi <- c()
+for (index in 1:12) {
+  local_nmi <- c()
+  for (count in 1:20) {
+    person.raw <- people[[index]][, 1:6]
+    k <- nlevels(factor(people[[index]]$Targets))
+    km <- kmeans(person.raw, k)
+    nmi <-
+      external_validation(people[[index]]$Targets, km$cluster, method = "nmi")
+    local_nmi <- c(local_nmi, nmi)
+  }
+  global_nmi <- c(global_nmi, mean(local_nmi))
+}
+
+# Binding everyone and then dividing
+everyone = people[[1]]
+for (i in 2:12) {
+  everyone = rbind(everyone, people[[i]])
+}
+
+ranges <-
+  list(
+    1:13831,
+    13832:28767,
+    28768:43994,
+    43995:56745,
+    56746:73753,
+    73754:88559,
+    88560:102332,
+    102333:117119,
+    117120:134759,
+    134760:158136,
+    158137:190836,
+    190837:207171
+  )
+everyone_nmi <- c()
+
+km.all <- kmeans(everyone[, 1:6], 5)
+for (range in ranges) {
+  nmi <-
+    external_validation(km.all$cluster[range], everyone$Targets[range], method = "nmi")
+  everyone_nmi <- c(everyone_nmi, nmi)
+}
+
+
+height <- rbind(global_nmi, everyone_nmi)
+
+colours <- c(4, 2)
+mp <-
+  barplot(
+    height,
+    beside = TRUE,
+    col = colours,
+    names.arg = 1:12,
+    main = "Grouped v. Individually Clustered",
+    ylab = "NMI",
+    xlab = "Patient",
+    legend.text = c(
+      paste("Individually clustered, mean: ", round(mean(global_nmi), 2), sep = ""),
+      paste("Grouped and clustered, mean: ", round(mean(everyone_nmi), 2), sep = "")
+    ),
+    args.legend = list(x = "top", cex = 0.75),
+    ylim = c(0, 1)
+  )
+
+text(
+  mp,
+  height,
+  labels = format(100 * round(height, 2)),
+  pos = 3,
+  cex = 0.6
+)
